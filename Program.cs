00d48dcf3580c79af9a6ae8f080f420be6589344
RadekChour <radek28.xd@seznam.cz>
2026-01-07 08:37:59 +0100

    hello

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int mojeCislo = 0;
+            float f = 43.5f;
+            double d = 43.3456;
+            
+            Console.WriteLine(mojeCislo);
+        }
+    }
+}
